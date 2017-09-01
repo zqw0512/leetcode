0b9eb1830515c75d220caf98b17f72a16939aca8
Unknown <qingwenzhu2000@gmail.com>
2017-08-31 22:16:20 -0700

    subsets

---
+++ subsets.java
@@ -0,0 +1,18 @@
+public class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+                List<List<Integer>> result = new ArrayList<>();
+                if (nums ==  null || nums.length == 0) {
+                        return result;
+                }
+                backtrack(result, new ArrayList<>(), nums, 0);
+                return result;
+        }
+        public void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, int start) {
+                result.add(new ArrayList<>(temp));
+                for(int i = start; i < nums.length; i++){
+                        temp.add(nums[i]);
+                        backtrack(result, temp, nums, i + 1);
+                        temp.remove(temp.size() - 1);
+                }
+        }
+}
